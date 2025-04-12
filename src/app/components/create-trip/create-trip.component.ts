1e455fed65fd104a40a3335ef3df0d95855a1122
menaesezobor <menaesezobor@gmail.com>
2025-04-12 23:50:03 +0100

    feat: implement reactive form for trip creation with validation controls

--- src/app/components/create-trip/create-trip.component.ts
+++ src/app/components/create-trip/create-trip.component.ts
@@ -1,27 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Form, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-create-trip',
   templateUrl: './create-trip.component.html',
-  styleUrls: ['./create-trip.component.css']
+  styleUrls: ['./create-trip.component.css'],
 })
 export class CreateTripComponent implements OnInit {
-onLocationSelect($event: Event) {
-throw new Error('Method not implemented.');
-}
-generateTrip() {
-throw new Error('Method not implemented.');
-}
-travelList: any;
-loading: any; 
-tripForm: any;
-budgetOptions= [
-  { title: 'Economy', icon: '💰', desc: 'Budget-friendly options' },
-  { title: 'Standard', icon: '💵', desc: 'Moderate spending' },
-  { title: 'Luxury', icon: '💎', desc: 'High-end experiences' },
-];
-  constructor() { }
-  ngOnInit(): void {
+  onLocationSelect($event: Event) {
+    throw new Error('Method not implemented.');
   }
-
+  generateTrip() {
+    throw new Error('Method not implemented.');
+  }
+  travelList: any;
+  loading: any;
+  tripForm: FormGroup = new FormGroup({
+    destination: new FormControl(''),
+    startDate: new FormControl(''),
+    endDate: new FormControl(''),
+    budget: new FormControl(''),
+    noOfDays: new FormControl(''),
+    travelList: new FormControl(''),
+    travelOptions: new FormControl(''),
+    travelDuration: new FormControl(''),
+    travelActivities: new FormControl(''),
+    travelBudget: new FormControl(''),
+  });
+  budgetOptions = [
+    { title: 'Economy', icon: '💰', desc: 'Budget-friendly options' },
+    { title: 'Standard', icon: '💵', desc: 'Moderate spending' },
+    { title: 'Luxury', icon: '💎', desc: 'High-end experiences' },
+  ];
+  constructor() {}
+  ngOnInit(): void {}
 }
