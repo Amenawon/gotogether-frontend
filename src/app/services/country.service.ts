5f0ce8274f26f1bbf45a8e51d77f0c94b33a5da6
menaesezobor <menaesezobor@gmail.com>
2025-05-25 23:10:45 +0100

    feat: add Country model and service for managing country data

---
+++ src/app/services/country.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Country } from '../model/country';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CountryService {
+
+  constructor(private httpClient:HttpClient) { }
+
+  getCountries() {
+    return this.httpClient.get<Country[]>(`${environment.url}/countries`);
+  }
+}
