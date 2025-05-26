a609c15264f0ef6eec9d6a30481441dfae4443c0
menaesezobor <menaesezobor@gmail.com>
2025-05-26 12:36:19 +0100

    chore: upgrade Angular dependencies to version 18.x and refactor AppModule to use provideHttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateTripModule } from 'src/app/components/create-trip/create-trip.module';
 import { CountryService } from './services/country.service';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
-@NgModule({
-  declarations: [
-    AppComponent],
-  imports: [
-    HttpClientModule,
-    BrowserModule,
-    AppRoutingModule,
-    CreateTripModule
-  ],
-  providers: [CountryService],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        CreateTripModule], providers: [CountryService, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
