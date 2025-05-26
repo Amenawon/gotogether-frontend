3b32bebaae4b1ceeb6601573e2395ddeb339c84c
menaesezobor <menaesezobor@gmail.com>
2025-05-26 11:48:14 +0100

    refactor: improve code formatting and add type annotations in CountryService

--- src/app/services/country.service.ts
+++ src/app/services/country.service.ts
@@ -2,15 +2,20 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Country } from '../model/country';
 import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountryService {
 
-  constructor(private httpClient:HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
-  getCountries() {
+  /**
+   * Fetches the list of countries from the server.
+   * @returns An observable of an array of Country objects.
+   */
+  getCountries():Observable<Country[]> {
     return this.httpClient.get<Country[]>(`${environment.url}/country/countries`);
   }
 }
